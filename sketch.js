59677b5111d631f0c449aed6d394153384a70586
agatsyaaghara <68064582+agatsyaaghara@users.noreply.github.com>
2020-08-26 05:35:31 -0700

    Add files via upload
    donot copy pls

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var gameState=0;
+var playerCount,database;
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database();
+   game1=new game()
+   game1.getState()
+   game1.state()
+}
+
+function draw(){
+   
+}
+
